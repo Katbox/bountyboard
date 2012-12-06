cde6d856c5d7cc63ebfd84db76d92b01362f53eb
Devin Leonhart <lionheartstudio@gmail.com>
2012-12-05 21:12:15 -0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bountyboard::Application.config.secret_token = '4ca79d1cb135d6e967d7d66b0e5d08e2811281a5d87b28f46fff4700412407d769bc4e865482088ba4b54df214cc915da7838099dbfcf04bc4135a3fc7f18a2a'
