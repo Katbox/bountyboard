78a864046fab2aa2f38af0cde6f0d93b10361015
Brent Houghton <slixbits@gmail.com>
2012-12-10 23:08:58 -0800

    Use more advanced rspec syntax to drastically clean up the existing view tests.

--- spec/features/board_spec.rb
+++ spec/features/board_spec.rb
@@ -4,37 +4,19 @@ require 'spec_helper'
 
 describe 'Board' do
 
+	subject { page }
+
 	describe "Home page" do
+		before { visit root_path }
 
-		it 'the Board view should be rendered in the application layout\'s .hero-unit div' do
-			visit root_path
-			page.should have_selector('.hero-unit h1',
-			  :text => 'Welcome to the Bounty Board')
-			page.should have_selector('.hero-unit h2',
-			  :text => 'A project that connects artists to fans and fans to the artwork they love.')
-		end
-
-		it 'the nav bar should be rendered at the top of the page' do
-			visit root_path
-			page.should have_selector('body > header.navbar.navbar-fixed-top.navbar-inverse:first-child')
-		end
-
-		it 'the sidebar should be rendered in the correct place' do
-			visit root_path
-			page.should have_selector('body > .container-fluid .row-fluid .nav-header',
-			  :text => 'Sidebar')
-		end
-
-		it 'the footer should be rendered in the correct place' do
-			visit root_path
-			page.should have_selector('body > .container-fluid:last-child footer.row-fluid')
-		end
-
-		it 'the footer should have a copyright declaration' do
-			visit root_path
-			page.should have_selector('body > .container-fluid:last-child footer.row-fluid',
-			  :text => '© Lionheart Studio and Brent Houghton')
-		end
+		it { should have_selector('.hero-unit h2',
+			  :text => 'A project that connects artists to fans and fans to the artwork they love.') }
+		it { should have_selector('body > header.navbar.navbar-fixed-top.navbar-inverse:first-child') }
+		it { should have_selector('body > .container-fluid .row-fluid .nav-header',
+			  :text => 'Sidebar') }
+		it { should have_selector('body > .container-fluid:last-child footer.row-fluid') }
+		it { should have_selector('body > .container-fluid:last-child footer.row-fluid',
+			  :text => '© Lionheart Studio and Brent Houghton') }
 	end
 end
 
