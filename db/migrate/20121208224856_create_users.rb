3a6286b5da229a1ad55afcb03b24ddc338b0698c
Devin Leonhart <lionheartstudio@gmail.com>
2013-01-08 18:15:58 -0800

    Removed permissions model and changed name validation

--- db/migrate/20121208224856_create_users.rb
+++ db/migrate/20121208224856_create_users.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string :name, :null => false
+    	t.string :name
     	t.string :email, :null => false
-    	t.integer :permission_id, :null => false
       	t.timestamps
     end
   end
