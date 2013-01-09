2ae25e265a15356f6de1565a835f73ce4552dd2a
Brent Houghton <slixbits@gmail.com>
2013-01-08 18:04:57 -0800

    Fill in logic for creating sessions after authenticating with Persona.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,33 @@ class SessionsController < ApplicationController
 	def create
 		
 		auth_info = request.env['omniauth.auth']
+
 		if not auth_info.uid
-			auth_failure
-		else
-			render :text => "You've successfully signed in as \"#{auth_info.uid}\" using the \"#{auth_info.provider}\" authentication provider.<br><br>Full user info from the provider:<br>#{auth_info}"
+			auth_failure(auth_info, "invalid credentials")
+			return
 		end
 
-		#@user = User.find_or_create_from_auth_hash(auth_hash)
-		#self.current_user = @user
-		#redirect_to '/'
+		user = User.find_by_email(auth_info.uid)
+		if not user
+			# if this user has never visited before, create a
+			# user entry for them
+			user = User.new(email: auth_info.uid)
+			if not user.save
+				auth_failure(auth_info, "this appears to be your first visit to our site, but our database rejected your account (#{user.errors.full_messages})")
+				return
+			end
+		end
+
+		auth_success(auth_info)
 	end
 
-	def auth_failure
-		render :text => 'Sign-in failed.'
-	end
+	private
+
+		def auth_failure(auth_info, message)
+			render :text => "Sign-in failed: #{message}"
+		end
+
+		def auth_success(auth_info)
+				render :text => "You've successfully signed in as \"#{auth_info.uid}\" using the \"#{auth_info.provider}\" authentication provider.<br><br>Full user info from the provider:<br>#{auth_info}"
+		end
 end
