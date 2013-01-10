63f9719c361da49432c31a0a7a82a4c7b1fcfe4a
Brent Houghton <slixbits@gmail.com>
2013-01-09 23:24:20 -0800

    Update the main site layout to support displaying Rails flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# generates the appropriate class name for flash messages when generating
+	# their HTML for the main application layout
+	def flash_class(level)
+		case level
+			when :notice then 'alert-info'
+			when :error then 'alert-error'
+			when :alert then '' # just use the default alert styling
+		end
+	end
 end
+
