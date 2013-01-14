7b6d001645332b9f61cceadd049280d2b4efc641
Brent Houghton <slixbits@gmail.com>
2013-01-13 19:51:38 -0800

    Update routes to support session handling.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Bountyboard::Application.routes.draw do
   # authentication routes
   match '/auth/:provider/callback', to: 'Sessions#create'
   match '/sign_out', to: 'Sessions#destroy', via: :delete
-  match '/auth/failure', to: 'Sessions#auth_failure'
+  match '/auth/failure', to: 'Sessions#authFailure'
 end
 
