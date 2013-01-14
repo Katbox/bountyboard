77b67fe5a953a37bf19d411a63b8b9df50d80ade
Brent Houghton <slixbits@gmail.com>
2013-01-13 17:57:28 -0800

    Add integration tests for the login process.

---
+++ spec/features/sessions_spec.rb
@@ -0,0 +1,54 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+describe 'Sessions' do
+
+	subject { page }
+
+	describe 'on failed login' do
+
+		before {
+			OmniAuth.config.mock_auth[:browser_id] = :invalid_credentials
+			visit '/auth/browser_id'
+		}
+
+		describe 'should display an error message on the home page' do
+
+			it { should have_selector('.alert.alert-error',
+				  :text => 'Sign-in failed: invalid credentials') }
+			it { should have_selector('.hero-unit .board-subheader',
+				  :text => 'A project that connects artists to fans and fans to the artwork they love.') }
+		end
+	end
+
+	describe 'on successful login' do
+
+		before {
+			OmniAuth.config.mock_auth[:browser_id] = OmniAuth::AuthHash.new({
+				:provider => 'browserid',
+				:uid => 'test-user@example.com'
+			})
+			visit '/auth/browser_id'
+		}
+
+		describe 'should display the home page' do
+
+			it { should have_selector('.hero-unit .board-header',
+				  :text => 'Welcome to the Bounty Board') }
+		end
+
+		describe 'should not display any login buttons' do
+			it { should_not have_selector('persona-login-button') }
+		
+		end
+
+		describe 'should show the Post Bounty button on the home page' do
+
+			it { should have_selector('.btn.btn-primary',
+				  :text => 'Post a Bounty') }
+		
+		end
+	end
+end
+
---
+++ spec/routing/sessions_routes.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+describe 'routing to sessions' do
+
+	it 'should route successful login to SessionsController#create' do
+		expect(:post => '/auth/browser_id/callback').to route_to(
+			:controller => 'sessions',
+			:action => 'create'
+		)
+	end
+
+	it 'should route session delete requests to SessionsController#signOut' do
+		expect(:post => '/auth/failure').to route_to(
+			:controller => 'sessions',
+			:action => 'authFailure'
+		)
+	end
+
+	it 'should route session delete requests to SessionsController#signOut' do
+		expect(:delete => '/sign_out').to route_to(
+			:controller => 'sessions',
+			:action => 'destroy'
+		)
+	end
+end
+
