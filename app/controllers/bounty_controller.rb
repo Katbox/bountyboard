4d0605f4c68a2bde08c1f3f2564fa166163ff453
Devin Leonhart <lionheartstudio@gmail.com>
2013-01-19 11:02:33 -0800

    Removed experimental code dealing with bounty controller. It turns out including bounty instance variables in the board controller is A-OK.

--- app/controllers/bounty_controller.rb
+++ app/controllers/bounty_controller.rb
@@ -2,12 +2,4 @@ class BountyController < ApplicationController
 	def index
 		@bounties = Bounty.all
 	end
-
-	def new
-		@bounty = Bounty.new
-	end
-
-	def showAll
-		@bounties = Bounty.all
-	end
 end
