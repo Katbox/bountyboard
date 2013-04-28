3c2bcf29cf001d15fab631fb5b8cfd13e68d8469
Brent Houghton <slixbits@gmail.com>
2013-04-27 17:31:31 -0700

    Add ArtistDetail model tests.

--- spec/models/artist_detail_spec.rb
+++ spec/models/artist_detail_spec.rb
@@ -17,5 +17,32 @@ describe ArtistDetail do
   it { should respond_to(:bountyRules) }
   it { should respond_to(:approved) }
 
+  it 'should only allow boolean values for its approved property' do
+    artist = FactoryGirl.build(:artist_detail, :approved => true)
+	artist.should be_valid
+	artist = FactoryGirl.build(:artist_detail, :approved => false)
+	artist.should be_valid
+	artist = FactoryGirl.build(:artist_detail, :approved => "cheese")
+	artist.approved.should == false
+  end
+
+  it 'should not allow null values for its approved property' do
+	artist = FactoryGirl.build(:artist_detail, :approved => nil)
+	artist.should_not be_valid
+	artist.should have(1).error_on(:approved)
+  end
+
+  it 'should not allow null values for its bio property' do
+    artist = FactoryGirl.build(:artist_detail, :bio => nil)
+	artist.should_not be_valid
+	artist.should have(1).error_on(:bio)
+  end
+
+  it 'should not allow null values for its bountyRules property' do
+    artist = FactoryGirl.build(:artist_detail, :bountyRules => nil)
+	artist.should_not be_valid
+	artist.should have(1).error_on(:bountyRules)
+  end
+
 end
 
