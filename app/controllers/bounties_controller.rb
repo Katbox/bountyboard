b4cafa35b1e946b582cbc7cbf8f5368ddbdc6a5c
Devin Leonhart <lionheartstudio@gmail.com>
2013-05-12 16:53:58 -0700

    Tabs -> Spaces

--- app/controllers/bounties_controller.rb
+++ app/controllers/bounties_controller.rb
@@ -1,53 +1,53 @@
 class BountiesController < ApplicationController
-	def index
-		@bounty = Bounty.all
-		@bounty.sort_by{|e| e[:created_at]}
-		@candidacy = Candidacy.all
-	end
+  def index
+    @bounty = Bounty.all
+    @bounty.sort_by{|e| e[:created_at]}
+    @candidacy = Candidacy.all
+  end
 
-	def show
-		# TODO: display the bounty
-		@bounty = Bounty.find(params[:id])
-	end
+  def show
+    # TODO: display the bounty
+    @bounty = Bounty.find(params[:id])
+  end
 
-	def new
-		@bounty = Bounty.new
-		@artists = ArtistDetail.all
-		@moods = Mood.all
-	end
+  def new
+    @bounty = Bounty.new
+    @artists = ArtistDetail.all
+    @moods = Mood.all
+  end
 
-	def create
-		#TODO Implement candidacy.
-		@bounty = Bounty.new(params[:bounty])
-		@bounty.user_id = 1
-		@candidacy = Candidacy.new()
-		@candidacy.bounty_id = @bounty.id
-		@candidacy.user_id = 1
-		@candidacy.save
-		if @bounty.save
-			redirect_to root_path
-		else
-			render 'new'
-		end
-	end
+  def create
+    #TODO Implement candidacy.
+    @bounty = Bounty.new(params[:bounty])
+    @bounty.user_id = 1
+    @candidacy = Candidacy.new()
+    @candidacy.bounty_id = @bounty.id
+    @candidacy.user_id = 1
+    @candidacy.save
+    if @bounty.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-		# TODO: display a page with a form for updating a bounty
-		# make sure only the user that created the bounty can use this method
-		bounty_to_update = Bounty.find(params[:id])
-	end
+  def edit
+    # TODO: display a page with a form for updating a bounty
+    # make sure only the user that created the bounty can use this method
+    bounty_to_update = Bounty.find(params[:id])
+  end
 
-	def update
-		# TODO: replace an existing bounty with a new one
-		# make sure only the user that created the bounty can use this method
-		bounty_to_replace = Bounty.find(params[:id])
-	end
+  def update
+    # TODO: replace an existing bounty with a new one
+    # make sure only the user that created the bounty can use this method
+    bounty_to_replace = Bounty.find(params[:id])
+  end
 
-	def destroy
-		# destroy the specified bounty
-		# TODO: make sure only the user that created the bounty can use this
-		# method
-		Bounty.destroy(params[:id])
-	end
+  def destroy
+    # destroy the specified bounty
+    # TODO: make sure only the user that created the bounty can use this
+    # method
+    Bounty.destroy(params[:id])
+  end
 end
 
