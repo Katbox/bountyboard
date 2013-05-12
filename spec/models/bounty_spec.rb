3a1d5c920932a77981ec51a1e06fa08e3a6d7f6e
Brent Houghton <slixbits@gmail.com>
2013-05-11 21:53:45 -0700

    Fix syntax errors introduced into the Bounty model tests by commit #243ef8.

--- spec/models/bounty_spec.rb
+++ spec/models/bounty_spec.rb
@@ -34,75 +34,75 @@ describe Bounty do
   }
 
   it 'should not allow null values for its name property' do
-  bounty = FactoryGirl.build(:bounty, :name => nil)
-  bounty.should_not be_valid
-  bounty.should have(2).error_on(:name)
+    bounty = FactoryGirl.build(:bounty, :name => nil)
+    bounty.should_not be_valid
+    bounty.should have(2).error_on(:name)
   end
 
   it 'should not allow name to be empty' do
-  bounty = FactoryGirl.build(:bounty, :name => '')
-  bounty.should_not be_valid
-  bounty.should have(2).error_on(:name)
+    bounty = FactoryGirl.build(:bounty, :name => '')
+    bounty.should_not be_valid
+    bounty.should have(2).error_on(:name)
   end
 
   it 'should not allow name to be too long' do
-  bounty = FactoryGirl.build(:bounty, :name => '$' * (Bounty.MAXIMUM_NAME_LENGTH + 1))
-  bounty.should_not be_valid
-  bounty.should have(1).error_on(:name)
+    bounty = FactoryGirl.build(:bounty, :name => '$' * (Bounty.MAXIMUM_NAME_LENGTH + 1))
+    bounty.should_not be_valid
+    bounty.should have(1).error_on(:name)
   end
 
   it 'should not allow null values for its desc property' do
-  bounty = FactoryGirl.build(:bounty, :desc => nil)
-  bounty.should_not be_valid
-  bounty.should have(2).error_on(:desc)
+    bounty = FactoryGirl.build(:bounty, :desc => nil)
+    bounty.should_not be_valid
+    bounty.should have(2).error_on(:desc)
   end
 
   it 'should not allow desc to be empty' do
-  bounty = FactoryGirl.build(:bounty, :desc => '')
-  bounty.should_not be_valid
-  bounty.should have(2).error_on(:desc)
+    bounty = FactoryGirl.build(:bounty, :desc => '')
+    bounty.should_not be_valid
+    bounty.should have(2).error_on(:desc)
   end
 
   it 'should not allow desc to be too long' do
-  bounty = FactoryGirl.build(:bounty, :desc => '$' * (Bounty.MAXIMUM_DESC_LENGTH + 1))
-  bounty.should_not be_valid
-  bounty.should have(1).error_on(:desc)
+    bounty = FactoryGirl.build(:bounty, :desc => '$' * (Bounty.MAXIMUM_DESC_LENGTH + 1))
+    bounty.should_not be_valid
+    bounty.should have(1).error_on(:desc)
   end
 
   it 'should not allow null values for its price_cents property' do
-  bounty = FactoryGirl.build(:bounty, :price_cents => nil)
-  bounty.should_not be_valid
-  bounty.should have(1).error_on(:price_cents)
+    bounty = FactoryGirl.build(:bounty, :price_cents => nil)
+    bounty.should_not be_valid
+    bounty.should have(1).error_on(:price_cents)
   end
 
   it 'should not allow null values for its rating property' do
-  bounty = FactoryGirl.build(:bounty, :rating => nil)
-  bounty.should_not be_valid
-  bounty.should have(1).error_on(:rating)
+    bounty = FactoryGirl.build(:bounty, :rating => nil)
+    bounty.should_not be_valid
+    bounty.should have(1).error_on(:rating)
   end
 
   it 'should only allow boolean values for its rating property' do
     bounty = FactoryGirl.build(:bounty, :rating => true)
-  bounty.should be_valid
-  bounty = FactoryGirl.build(:bounty, :rating => false)
-  bounty.should be_valid
-  bounty = FactoryGirl.build(:bounty, :rating => "cheese")
-  bounty.rating.should == false
+    bounty.should be_valid
+    bounty = FactoryGirl.build(:bounty, :rating => false)
+    bounty.should be_valid
+    bounty = FactoryGirl.build(:bounty, :rating => "cheese")
+    bounty.rating.should == false
   end
 
   it 'should not allow null values for its is_private property' do
-  bounty = FactoryGirl.build(:bounty, :is_private => nil)
-  bounty.should_not be_valid
-  bounty.should have(1).error_on(:is_private)
+    bounty = FactoryGirl.build(:bounty, :is_private => nil)
+    bounty.should_not be_valid
+    bounty.should have(1).error_on(:is_private)
   end
 
   it 'should only allow boolean values for its is_private property' do
     bounty = FactoryGirl.build(:bounty, :is_private => true)
-  bounty.should be_valid
-  bounty = FactoryGirl.build(:bounty, :is_private => false)
-  bounty.should be_valid
-  bounty = FactoryGirl.build(:bounty, :is_private => "cheese")
-  bounty.private.should == false
+    bounty.should be_valid
+    bounty = FactoryGirl.build(:bounty, :is_private => false)
+    bounty.should be_valid
+    bounty = FactoryGirl.build(:bounty, :is_private => "cheese")
+    bounty.private.should == false
+  end
 end
-
 
