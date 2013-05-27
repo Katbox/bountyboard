9bbad8d20f21bbe8af36c392080b4fd478836d5a
Devin Leonhart <lionheartstudio@gmail.com>
2013-05-27 16:12:34 -0700

    Added test for show. Will flesh this out once interface is done.

--- spec/features/bounty_spec.rb
+++ spec/features/bounty_spec.rb
@@ -59,21 +59,40 @@ describe 'Bounty' do
       @artist1 = FactoryGirl.create(:artist, :name => "Artist1")
       @artist2 = FactoryGirl.create(:artist, :name => "Artist2")
       @artist3 = FactoryGirl.create(:artist, :name => "Artist3")
-      @artist3 = FactoryGirl.create(:artist, :name => "Artist4")
-
+      @artist4 = FactoryGirl.create(:artist, :name => "Artist4")
+      @mood = FactoryGirl.create(:mood, :name => "Mood1")
       visit new_bounty_path
     }
 
     describe 'should display a form to create a bounty' do
-      it { should have_selector('#bounty-post-area')}
+      it { should have_selector('#bounty-page-area')}
       it { should have_selector('#bounty_artists_input') }
       it { should have_selector('label[for=bounty_artist_ids_1]', :text => "Artist1") }
       it { should have_selector('label[for=bounty_artist_ids_2]', :text => "Artist2") }
       it { should have_selector('label[for=bounty_artist_ids_3]', :text => "Artist3") }
       it { should have_selector('label[for=bounty_artist_ids_4]', :text => "Artist4") }
       it { should have_selector('#bounty_moods_input') }
-      it { should have_selector('label[for=bounty_mood_ids_1]', :text => "mood1") }
+      it { should have_selector('label[for=bounty_mood_ids_1]', :text => "Mood1") }
+    end
+  end
 
+  describe "show bounty" do
+    before {
+      @user = FactoryGirl.create(:user)
+      @artist = FactoryGirl.create(:artist)
+      @bounty = FactoryGirl.create(:bounty,
+        :name => "My First Bounty",
+        :desc => "My very first bounty ever.",
+        :price => 9.99,
+        :rating => false,
+        :is_private => false,
+        :user_id => @user.id
+        )
+      visit bounty_path(@bounty.id)
+    }
+    describe 'should display a form to create a bounty' do
+      it { should have_selector('#bounty-page-area')}
+      #TODO Create tests for selectors and content when inerface is redone.
     end
   end
 
