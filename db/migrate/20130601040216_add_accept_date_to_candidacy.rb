7bee3ca098047c82c9db944cd06eb6d40c1956ce
Devin Leonhart <lionheartstudio@gmail.com>
2013-05-31 21:11:27 -0700

    Added migration that will add timestamp to accept

---
+++ db/migrate/20130601040216_add_accept_date_to_candidacy.rb
@@ -0,0 +1,9 @@
+class AddIsAdminFlagToUser < ActiveRecord::Migration
+  def up
+    add_column :candidacies, :accepted_at, :datetime
+  end
+
+  def down
+    remove_column :candidacies, :accepted_at
+  end
+end
