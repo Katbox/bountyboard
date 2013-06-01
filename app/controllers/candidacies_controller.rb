ae3277fc27c05f14ae0b6fd0f0d13b45eb14e4d5
Devin Leonhart <lionheartstudio@gmail.com>
2013-05-31 20:58:21 -0700

    Updated candidacy controller logic with new helpers

--- app/controllers/candidacies_controller.rb
+++ app/controllers/candidacies_controller.rb
@@ -19,40 +19,33 @@ class CandidaciesController < ApplicationController
 
   def update
     @bounty = Bounty.find(params[:id])
-
-    if currentUser.type == 'Artist'
-      @candidacy = Candidacy.where(:artist_id => currentUser.id, :bounty_id => @bounty.id).first
-      if @candidacy == nil
-        redirect_to root_path, :error => "You are not a candidate for that bounty."
+    redirect_to root_path, :error => "Only artists may accept bounties." if (currentUser.is_artist? == false)
+    @candidacy = Candidacy.where(:artist_id => currentUser.id, :bounty_id => @bounty.id).first
+    unless @candidacy.nil?
+      if @candidacy.update_attributes(params[:candidacy])
+        redirect_to root_path, :notice => "Bounty Accepted!"
       else
-        if @candidacy.update_attributes(params[:candidacy])
-          redirect_to root_path, :notice => "Bounty Accepted!"
-        else
-          redirect_to edit_bounty_path(@bounty.id), :error => "Error Accepting bounty."
-        end
+        redirect_to edit_bounty_path(@bounty.id), :error => "Error Accepting bounty."
       end
     else
-      redirect_to root_path, :error => "Only artists may accept bounties."
+      redirect_to root_path, :error => "You are not a candidate for that bounty."
     end
   end
 
   def destroy
     @bounty = Bounty.find(params[:id])
-
-    if currentUser.type == 'Artist'
-      @candidacy = Candidacy.where(:artist_id => currentUser.id, :bounty_id => @bounty.id).first
-      if @candidacy == nil
-        redirect_to root_path, :error => "You are not a candidate for that bounty."
+    redirect_to root_path, :error => "Only artists may reject bounties." if (currentUser.is_artist? == false)
+    @candidacy = Candidacy.where(:artist_id => currentUser.id, :bounty_id => @bounty.id).first
+    unless @candidacy.nil?
+      if @candidacy.destroy
+        redirect_to root_path, :notice => "Candidacy Removed!"
       else
-        if @candidacy.destroy
-          redirect_to root_path, :notice => "Candidacy Removed!"
-        else
-          redirect_to edit_bounty_path(@bounty.id), :error => "Error Removing Candidacy."
-        end
+        redirect_to edit_bounty_path(@bounty.id), :error => "Error Removing Candidacy."
       end
     else
-      redirect_to root_path, :error => "Only artists may reject bounties."
+      redirect_to root_path, :error => "You are not a candidate for that bounty."
     end
   end
+
 end
 
