de712c45befcab19a41c922fb610d2a5d412e97c
Devin Leonhart <lionheartstudio@gmail.com>
2013-06-08 11:43:59 -0700

    Form styling. Not finished.

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
 # Set the default text field size when input is a string. Default is nil.
-# Formtastic::FormBuilder.default_text_field_size = 50
+Formtastic::FormBuilder.default_text_field_size = 50
 
 # Set the default text area height when input is a text. Default is 20.
-# Formtastic::FormBuilder.default_text_area_height = 5
+Formtastic::FormBuilder.default_text_area_height = 5
 
 # Set the default text area width when input is a text. Default is nil.
-# Formtastic::FormBuilder.default_text_area_width = 50
+Formtastic::FormBuilder.default_text_area_width = 55
 
 # Should all fields be considered "required" by default?
 # Defaults to true.
@@ -22,7 +22,7 @@
 # '<abbr title="required">*</abbr>'. In other words, if you configure formtastic.required
 # in your locale, it will replace the abbr title properly. But if you don't want to use
 # abbr tag, you can simply give a string as below
-# Formtastic::FormBuilder.required_string = "(required)"
+Formtastic::FormBuilder.required_string = ""
 
 # Set the string that will be appended to the labels/fieldsets which are optional
 # Defaults to an empty string ("") and also accepts procs (see required_string above)
