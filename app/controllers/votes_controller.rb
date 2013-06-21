63abe5ad997c614a3b701851519278e7f8f8f46e
Devin Leonhart <lionheartstudio@gmail.com>
2013-06-21 12:19:32 -0700

    Added confidence function from online example

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,21 @@
 class VotesController < ApplicationController
 
- include SessionsHelper
+  require 'statistics2'
+  include SessionsHelper
 
   def create
   end
 
   def destroy
+  end
+
+  def calculateConfidence(pos, n, confidence)
+    if n == 0
+      return 0
+    end
+    z = Statistics2.pnormaldist(1-(1-confidence)/2)
+    phat = 1.0*pos/n
+    (phat + z*z/(2*n) - z * Math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
   end
 
 end
