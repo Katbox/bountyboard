be6ec8c451f7daa97bceb01cd4eb9cb63eb40c9a
Devin Leonhart <lionheartstudio@gmail.com>
2013-06-21 14:39:43 -0700

    Missing comma

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -13,7 +13,7 @@ class VotesController < ApplicationController
 
         #Variables required for "Lower bound of Wilson score confidence interval
         #for a Bernoulli parameter"
-        pos = Vote.where(:bounty_id => @bounty.id :vote_type => true).count
+        pos = Vote.where(:bounty_id => @bounty.id, :vote_type => true).count
         n = Vote.where(:bounty_id => @bounty.id).count
         #Magic number indicating 95% confidence level.
         confidence = 0.95
