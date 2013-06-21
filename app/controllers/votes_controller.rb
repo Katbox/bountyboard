d5799ba03609978e0dbdc2ef693c309abf2ee251
Devin Leonhart <lionheartstudio@gmail.com>
2013-06-21 12:04:13 -0700

    Added statistics2 gem and updated other gems

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,22 +2,7 @@ class VotesController < ApplicationController
 
  include SessionsHelper
 
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
   def create
-  end
-
-  def edit
-  end
-
-  def update
   end
 
   def destroy
