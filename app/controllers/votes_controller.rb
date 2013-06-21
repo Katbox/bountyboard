69ac6340c1eaa725338611a2aae6bce3758e684f
Devin Leonhart <lionheartstudio@gmail.com>
2013-06-21 13:07:53 -0700

    Wired up voting buttons to create action for votes. Added create action

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,6 +4,25 @@ class VotesController < ApplicationController
   include SessionsHelper
 
   def create
+    if signed_in?
+      @vote = Vote.new(params[:vote])
+      @vote.user_id = currentUser.id
+      @bounty = Bounty.find(params[:bounty])
+      @vote.bounty_id = @bounty.id
+      if @vote.save
+        if params[:vote][:vote_type]
+          flash[:notice] = "You have upvoted #{@bounty.name}!"
+        else
+          flash[:notice] = "You have downvoted #{@bounty.name}!"
+        end
+        redirect_to root_path
+      else
+        flash[:error] = "Error saving vote."
+        redirect_to root_path
+      end
+    else
+      flash[:error] = "You must sign in."
+      redirect_to root_path
   end
 
   def destroy
