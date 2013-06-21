b363e857e66af42d53eadc192f2bb9e78b17096c
Devin Leonhart <lionheartstudio@gmail.com>
2013-06-21 14:40:00 -0700

    Mistakenly used rating instead of score

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -117,7 +117,7 @@ class Bounty < ActiveRecord::Base
     end
     z = Statistics2.pnormaldist(1-(1-confidence)/2)
     phat = 1.0*pos/n
-    self[:rating] = (phat + z*z/(2*n) - z * Math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
+    self[:score] = (phat + z*z/(2*n) - z * Math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
   end
 
   # Returns private or public based on the boolean private property.
