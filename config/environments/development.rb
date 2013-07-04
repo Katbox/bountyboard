a7891ebb4fb617f6b8b5e31d6aae9fd0057d2928
Brent Houghton <slixbits@gmail.com>
2013-07-04 16:07:19 -0700

    Provide a line that can be uncommented to enable testing caching in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,9 @@ Bountyboard::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # enable the Rails cache in development (comment this out again before committing)
+  # config.action_controller.perform_caching = true
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
