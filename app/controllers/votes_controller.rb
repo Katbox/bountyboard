4bd0d76c6fce240ce4035eb454c6e3a1bdf2b0b0
Devin Leonhart <lionheartstudio@gmail.com>
2013-07-04 22:20:53 -0700

    Implemented vote#destroy

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -24,4 +24,18 @@ class VotesController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def destroy
+    if signed_in?
+      if Vote.destroy(params[:id])
+        redirect_to root_path
+      else
+        flash[:error] = "Error removing vote."
+        redirect_to root_path
+      end
+    else
+      flash[:error] = "You must sign in."
+      redirect_to root_path
+    end
+  end
 end
