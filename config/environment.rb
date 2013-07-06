278d434aec2c497e9d12e87848c83ce9c3017ab6
Brent Houghton <slixbits@gmail.com>
2013-07-06 16:18:29 -0700

    Move Bounty timestamp formatting from a config file to the Bounty#show view.

--- config/environment.rb
+++ config/environment.rb
@@ -2,13 +2,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Adding in our custom time formatting
-
-Time::DATE_FORMATS[:created_on] = "Created on %B %d %Y"
-Time::DATE_FORMATS[:accepted_on] = "Accepted on %B %d %Y"
-Time::DATE_FORMATS[:completed_on] = "Completed on %B %d %Y"
-Time::DATE_FORMATS[:joined_on] = "Joined on %B %d %Y"
-Time::DATE_FORMATS[:complete_by] = "This bounty must be finished by %B %d %Y"
-
 # Initialize the rails application
 Bountyboard::Application.initialize!
