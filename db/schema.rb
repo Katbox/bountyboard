317066447b4f9c9906eea3b158e959748d94e6a1
Brent Houghton <slixbits@gmail.com>
2013-07-09 15:39:40 -0700

    Rebuild the database, generating a Rails 4-style schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -8,76 +9,76 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130707061659) do
+ActiveRecord::Schema.define(version: 20130707061659) do
 
-  create_table "bounties", :force => true do |t|
-    t.string   "name",                              :null => false
-    t.text     "desc",                              :null => false
-    t.integer  "price_cents",    :default => 0,     :null => false
-    t.string   "price_currency", :default => "USD", :null => false
-    t.boolean  "adult_only",     :default => false, :null => false
-    t.boolean  "private",        :default => false, :null => false
+  create_table "bounties", force: true do |t|
+    t.string   "name",                           null: false
+    t.text     "desc",                           null: false
+    t.integer  "price_cents",    default: 0,     null: false
+    t.string   "price_currency", default: "USD", null: false
+    t.boolean  "adult_only",     default: false, null: false
+    t.boolean  "private",        default: false, null: false
     t.string   "url"
-    t.integer  "user_id",                           :null => false
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.integer  "user_id",                        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "completed_at"
     t.datetime "complete_by"
-    t.string   "tag_line",                          :null => false
+    t.string   "tag_line",                       null: false
   end
 
-  create_table "candidacies", :force => true do |t|
-    t.integer  "bounty_id",   :null => false
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.integer  "artist_id",   :null => false
+  create_table "candidacies", force: true do |t|
+    t.integer  "bounty_id",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "artist_id",   null: false
     t.datetime "accepted_at"
   end
 
-  create_table "favorites", :force => true do |t|
-    t.integer  "user_id",    :null => false
-    t.integer  "bounty_id",  :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "bounty_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "moods", :force => true do |t|
-    t.string   "name",       :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "moods", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "personalities", :force => true do |t|
-    t.integer  "mood_id",    :null => false
-    t.integer  "bounty_id",  :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "personalities", force: true do |t|
+    t.integer  "mood_id",    null: false
+    t.integer  "bounty_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email",                            :null => false
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
+    t.string   "email",                         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "rememberToken"
     t.string   "type"
-    t.text     "bio",           :default => "",    :null => false
-    t.text     "bounty_rules",  :default => "",    :null => false
-    t.boolean  "approved",      :default => false, :null => false
-    t.boolean  "admin",         :default => false, :null => false
+    t.text     "bio",           default: "",    null: false
+    t.text     "bounty_rules",  default: "",    null: false
+    t.boolean  "approved",      default: false, null: false
+    t.boolean  "admin",         default: false, null: false
     t.boolean  "active"
   end
 
-  add_index "users", ["rememberToken"], :name => "index_users_on_rememberToken"
+  add_index "users", ["rememberToken"], name: "index_users_on_rememberToken"
 
-  create_table "votes", :force => true do |t|
-    t.integer  "user_id",                       :null => false
-    t.integer  "bounty_id",                     :null => false
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-    t.boolean  "vote_type",  :default => false, :null => false
+  create_table "votes", force: true do |t|
+    t.integer  "user_id",                    null: false
+    t.integer  "bounty_id",                  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "vote_type",  default: false, null: false
   end
 
 end
